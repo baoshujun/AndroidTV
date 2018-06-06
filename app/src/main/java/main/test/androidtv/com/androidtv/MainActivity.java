db91f6598d61384dbb157fe1c0226a7ea5f84191
baoshujun <baoshujun@baoshujundeMacBook-Pro.local>
2018-06-06 10:08:53 +0800

    Initial commit

---
+++ app/src/main/java/main/test/androidtv/com/androidtv/MainActivity.java
@@ -0,0 +1,17 @@
+package main.test.androidtv.com.androidtv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
